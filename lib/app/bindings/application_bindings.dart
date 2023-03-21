48ef5ffab7a17f308248064cec6822309b0ecde5
marco-rozo <marcorozo99@gmail.com>
2023-03-21 13:34:36 -0300

    chore: add application binding

---
+++ lib/app/bindings/application_bindings.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:idr_mobile/app/data/providers/api/rest_client.dart';
+import 'package:idr_mobile/app/data/repositories/login/login_repository.dart';
+import 'package:idr_mobile/app/data/repositories/login/login_repository_impl.dart';
+import 'package:idr_mobile/app/data/services/auth/auth_service.dart';
+import 'package:idr_mobile/app/data/services/login/login_service.dart';
+import 'package:idr_mobile/app/data/services/login/login_service_impl.dart';
+
+class ApplicationBindings implements Bindings {
+  @override
+  void dependencies() {}
+}
