3b090b74104b34ea63a24b65ff684968ca994b4b
marco-rozo <marcorozo99@gmail.com>
2023-03-21 18:45:16 -0300

    feat: changing main to work with GetMaterialApp from get package

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:idr_mobile/routes/app_pages.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
+  await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(MyApp());
@@ -17,10 +18,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //  AuthService? auth = Get.find<AuthService>();
 
-    return MaterialApp(
-      title: "Flutter App",
+    return GetMaterialApp(
+      title: UIConfig.title,
+      theme: UIConfig.theme,
+      initialBinding: ApplicationBindings(),
+      getPages: AppPages.pages,
+      // initialRoute: auth.isLogged() ? Routes.HOME : Routes.LOGIN,
+      initialRoute: Routes.LOGIN,
+      themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
-      home: Container(),
     );
   }
 }
