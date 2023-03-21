451971046a5fe93b49d984c0ab2ca1db8abcd3ab
marco-rozo <marcorozo99@gmail.com>
2023-03-21 18:47:09 -0300

    chore: add dependencies in bindings file

--- lib/app/bindings/application_bindings.dart
+++ lib/app/bindings/application_bindings.dart
@@ -8,5 +8,29 @@ import 'package:idr_mobile/app/data/services/login/login_service_impl.dart';
 
 class ApplicationBindings implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    // Entrego o gerenciamento das dependecias para o GetX administrar
+    // .lazyPut para dependencias que são necessárias mas só "precisam" ser carregaadas/encontradas
+    // quando forem solicitadas
+    // .put para dependencias que precisam serem "verificadas"/carregaadas/encontradas/INSTANCIADAS "logo"
+    //para torná-la disponível para todas as rotas "filhos".
+    Get.lazyPut(
+      () => RestClient(),
+      fenix: true,
+    );
+
+    //classes que "não" finalizam durante o tempo de vida da aplicação dado "fenix: true"
+    Get.lazyPut<LoginRepository>(
+      () => LoginRepositoryImpl(restClient: Get.find()),
+      fenix: true,
+    );
+
+    Get.lazyPut<LoginService>(
+      () => LoginServiceImpl(loginRepository: Get.find()),
+      fenix: true,
+    );
+
+    //ao carregar a aplicação verifica se está logado ou nao e faz o direcionamento para a rota inicial
+    Get.put(AuthService()).init();
+  }
 }
