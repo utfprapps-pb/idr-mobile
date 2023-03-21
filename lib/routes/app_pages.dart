ba155434788101e9ac5cbbfb116b8621e2adb497
marco-rozo <marcorozo99@gmail.com>
2023-03-21 18:44:23 -0300

    feat: create routes

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+import 'package:idr_mobile/app/modules/login/login_binding.dart';
+import 'package:idr_mobile/app/modules/login/login_page.dart';
+
+part './routes.dart';
+
+abstract class AppPages {
+  //Lista de páginas com suas rotas definidas
+  static final pages = [
+    GetPage(
+      name: Routes.LOGIN,
+      page: () => LoginPage(),
+      bindings: [
+        LoginBinding(),
+      ],
+    ),
+    GetPage(
+      name: Routes.HOME,
+      page: () => LoginPage(),
+      bindings: [
+        LoginBinding(),
+      ],
+    ),
+  ];
+}
