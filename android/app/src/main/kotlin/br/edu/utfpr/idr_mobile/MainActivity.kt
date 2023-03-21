9e55e30afb8388a93d0c8b0b9e75dabd6c36fd6a
marco-rozo <marcorozo99@gmail.com>
2023-03-21 13:24:19 -0300

    init project

---
+++ android/app/src/main/kotlin/br/edu/utfpr/idr_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.edu.utfpr.idr_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:idr_mobile/app/bindings/application_bindings.dart';
+import 'package:idr_mobile/app/data/services/auth/auth_service.dart';
+import 'package:idr_mobile/core/theme/ui_config.dart';
+import 'package:idr_mobile/routes/app_pages.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //  AuthService? auth = Get.find<AuthService>();
+
+    return MaterialApp(
+      title: "Flutter App",
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
+  }
+}
