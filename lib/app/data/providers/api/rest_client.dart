c17e6f88affa3c6aed2d3ea2132c85db96bf7d98
marco-rozo <marcorozo99@gmail.com>
2023-03-23 23:53:13 -0300

    chore: changed baseurl to work in emulator

--- lib/app/data/providers/api/rest_client.dart
+++ lib/app/data/providers/api/rest_client.dart
@@ -3,10 +3,11 @@ import 'dart:convert';
 import 'package:get/get.dart';
 
 //baseUrl para conectar em localhost via emulador
-const baseUrl = '10.2.2.2:8080';
+const baseUrl = 'http://10.0.2.2:8080/';
 
 class RestClient extends GetConnect {
   RestClient() {
     httpClient.baseUrl = baseUrl;
+    httpClient.timeout = Duration(seconds: 60);
   }
 }
