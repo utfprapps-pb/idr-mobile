234d034fa67ac2a45897bab259b5297dff034880
marco-rozo <marcorozo99@gmail.com>
2023-03-24 00:00:14 -0300

    chore (style): added icon button and input borders

--- lib/app/widgets/custom_input_field.dart
+++ lib/app/widgets/custom_input_field.dart
@@ -8,31 +8,36 @@ class CustomInputField extends StatelessWidget {
   final FocusNode? inputFocus;
   final TextEditingController inputController;
   final VoidCallback onTapCallBack;
+  final VoidCallback? onPressedIcon;
   final String labelText;
   final IconData? icon;
   final TextInputType? keyboardType;
   final List<TextInputFormatter>? inputFormatters;
-  final bool? isObbscureText;
+  final bool? isObscureText;
   final bool? isEnable;
   final String? Function(String?)? validator;
+  final Function(String?)? onChanged;
 
   const CustomInputField({
     super.key,
     this.inputFocus,
     required this.inputController,
     required this.onTapCallBack,
+    this.onPressedIcon,
     required this.labelText,
     this.icon,
     this.keyboardType,
     this.inputFormatters,
-    this.isObbscureText,
+    this.isObscureText,
     this.isEnable,
     this.validator,
+    this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onChanged: onChanged,
       validator: validator,
       controller: inputController,
       style: TextStyle(
@@ -43,7 +48,7 @@ class CustomInputField extends StatelessWidget {
       onTap: () => onTapCallBack,
       keyboardType: keyboardType,
       inputFormatters: inputFormatters ?? [],
-      obscureText: isObbscureText ?? false,
+      obscureText: isObscureText ?? false,
       cursorColor: UIColors.primaryColor,
       decoration: InputDecoration(
           // constraints: BoxConstraints(minHeight: 50),
@@ -54,48 +59,56 @@ class CustomInputField extends StatelessWidget {
           labelStyle: TextStyle(
             color: UIColors.primaryColor,
           ),
-          suffixIcon: icon != null
-              ? Icon(
-                  icon,
-                  color: UIColors.primaryColor,
-                  size: 20,
-                )
-              : null,
-          // errorBorder: OutlineInputBorder(
-          //   borderSide: BorderSide(
-          //     color: UIColors.dangerColor,
-          //     width: 1,
-          //   ),
-          //   borderRadius: BorderRadius.circular(10),
-          // ),
-          // focusedErrorBorder: OutlineInputBorder(
-          //   borderSide: BorderSide(
-          //     color: UIColors.primaryColor,
-          //     width: 1,
-          //   ),
-          //   borderRadius: BorderRadius.circular(10),
-          // ),
-          // enabledBorder: OutlineInputBorder(
-          //   borderSide: BorderSide(
-          //     color: UIColors.primaryColor,
-          //     width: 1,
-          //   ),
-          //   borderRadius: BorderRadius.circular(10),
-          // ),
-          // disabledBorder: OutlineInputBorder(
-          //   borderSide: BorderSide(
-          //     color: UIColors.disableColor,
-          //     width: 1,
-          //   ),
-          //   borderRadius: BorderRadius.circular(10),
-          // ),
-          // focusedBorder: OutlineInputBorder(
-          //   borderSide: BorderSide(
-          //     color: UIColors.primaryColor,
-          //     width: 1.5,
-          //   ),
-          //   borderRadius: BorderRadius.circular(10),
-          // ),
+          suffixIcon: onPressedIcon != null
+              ? IconButton(
+                  onPressed: onPressedIcon,
+                  icon: Icon(
+                    icon,
+                    color: UIColors.primaryColor,
+                    size: 20,
+                  ))
+              : icon != null
+                  ? Icon(
+                      icon,
+                      color: UIColors.primaryColor,
+                      size: 20,
+                    )
+                  : null,
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: UIColors.dangerColor,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: UIColors.primaryColor,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: UIColors.primaryColor,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: UIColors.disableColor,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: UIColors.primaryColor,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
           border: OutlineInputBorder(
             borderSide: BorderSide(
               color: UIColors.primaryColor,
