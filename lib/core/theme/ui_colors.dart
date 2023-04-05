a2bf1c9f81c6a90f326c838a94d491d86835d2b3
marco-rozo <marcorozo99@gmail.com>
2023-04-04 23:57:24 -0300

    chore: add new colors

--- lib/core/theme/ui_colors.dart
+++ lib/core/theme/ui_colors.dart
@@ -6,7 +6,8 @@ class UIColors {
   static Color get primaryColor => const Color(0xFF0059AA);
   static Color get secondaryColor => const Color(0xFF00854A);
   static Color get whiteColor => const Color(0xFFF8F8FF);
-  static Color get blackColor => const Color(0xFF121212);
+  static Color get blackColor => const Color(0xFF1C1C1C);
+  static Color get blackColor24 => const Color(0xFF242424);
   static Color get dangerColor => const Color(0xFFff4444);
   static Color get warningColor => const Color(0xFFffbb33);
   static Color get successColor => const Color(0xFF00C851);
