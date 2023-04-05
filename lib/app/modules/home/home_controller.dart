03942ab83efdba2be00084da3aa94a49204737d1
marco-rozo <marcorozo99@gmail.com>
2023-04-05 13:33:11 -0300

    chore: add property service in home controller

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -3,14 +3,18 @@ import 'package:idr_mobile/app/data/repositories/login/login_repository.dart';
 import 'package:idr_mobile/app/data/services/auth/auth_service.dart';
 import 'package:idr_mobile/app/data/services/home/home_service.dart';
 import 'package:idr_mobile/app/data/services/login/login_service.dart';
+import 'package:idr_mobile/app/data/services/property/property_service.dart';
 import 'package:idr_mobile/routes/app_pages.dart';
 
 class HomeController extends GetxController {
   final HomeService _homeService;
+  final PropertyService _propertyService;
 
   HomeController({
     required HomeService homeService,
-  }) : _homeService = homeService;
+    required PropertyService propertyService,
+  })  : _homeService = homeService,
+        _propertyService = propertyService;
 
   AuthService? auth;
   RxString displayName = ''.obs;
