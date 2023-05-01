a09b4b0e89b1469ab0db9c1651c74d759a2554c5
marco-rozo <marcorozo99@gmail.com>
2023-05-01 18:33:34 -0300

    fix: changed success end danger colors

--- lib/core/theme/ui_colors.dart
+++ lib/core/theme/ui_colors.dart
@@ -8,9 +8,9 @@ class UIColors {
   static Color get whiteColor => const Color(0xFFF8F8FF);
   static Color get blackColor => const Color(0xFF1C1C1C);
   static Color get blackColor24 => const Color(0xFF242424);
-  static Color get dangerColor => const Color(0xFFff4444);
+  static Color get dangerColor => Colors.red[400] ?? Color(0xFFff4444);
   static Color get warningColor => const Color(0xFFffbb33);
-  static Color get successColor => const Color(0xFF00C851);
+  static Color get successColor => Colors.green[400] ?? Color(0xFF00C851);
   static Color get infoColor => const Color(0xFF33b5e5);
   static Color get disableColor => const Color(0xFFC7C7C7);
 }
