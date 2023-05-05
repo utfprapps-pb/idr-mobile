2ff56646a91a6077c75376701d6adbf67368d644
marco-rozo <marcorozo99@gmail.com>
2023-05-05 12:59:48 -0300

    feat: save property selected in storage

--- lib/app/data/services/auth/auth_service.dart
+++ lib/app/data/services/auth/auth_service.dart
@@ -1,34 +1,40 @@
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:idr_mobile/app/data/models/property_model.dart';
 import 'package:idr_mobile/app/data/models/user_model.dart';
 import 'package:idr_mobile/core/values/consts.dart';
 import 'package:idr_mobile/routes/app_pages.dart';
 
 class AuthService extends GetxService {
-  late GetStorage box;
+  late GetStorage storage;
 
   void init() async {
-    box = GetStorage();
+    storage = GetStorage();
 
     //Caso seja null seta como falso
-    await box.writeIfNull(IS_LOGGED, false);
-    await box.writeIfNull(TOKEN, '');
-    await box.writeIfNull(DISPLAY_NAME, '');
+    await storage.writeIfNull(IS_LOGGED, false);
+    await storage.writeIfNull(TOKEN, '');
+    await storage.writeIfNull(DISPLAY_NAME, '');
+    await storage.writeIfNull(PROPERTY_SELECTED, '');
   }
 
   //Retorna a leitura da chave local
-  isLogged() => box.read(IS_LOGGED);
-  apiToken() => box.read(TOKEN);
-  displayName() => box.read(DISPLAY_NAME);
+  isLogged() => storage.read(IS_LOGGED);
+  apiToken() => storage.read(TOKEN);
+  displayName() => storage.read(DISPLAY_NAME);
+  property() => storage.read(PROPERTY_SELECTED);
 
-  changeIsLogged(bool isLogged) async => box.write(IS_LOGGED, isLogged);
-  changeApiToken(String token) async => box.write(TOKEN, token);
+  changeIsLogged(bool isLogged) async => storage.write(IS_LOGGED, isLogged);
+  changeApiToken(String token) async => storage.write(TOKEN, token);
   changeDisplayName(String displayName) async =>
-      box.write(DISPLAY_NAME, displayName);
+      storage.write(DISPLAY_NAME, displayName);
+  changeProperty(PropertyModel? property) async =>
+      storage.write(PROPERTY_SELECTED, property);
 
   logout() async => {
         changeIsLogged(false),
         changeApiToken(''),
         changeDisplayName(''),
+        changeProperty(null),
       };
 }
