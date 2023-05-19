7e0973cde8c9aaba9ca1ff5010240f1bcae29331
marco-rozo <marcorozo99@gmail.com>
2023-05-18 23:14:50 -0300

    feat: create mastitis repository

---
+++ lib/app/data/repositories/mastitis/mastitis_repository.dart
@@ -0,0 +1,10 @@
+import 'package:idr_mobile/app/data/models/mastitis_model.dart';
+
+abstract class MastitisRepository {
+  Future<List<MastitisModel>> getAllMastitis();
+  Future<List<MastitisModel>> getAllMastitisInDb(String? animalIdentifier);
+  Future<bool> saveMastitisInDb(MastitisModel mastitis);
+  Future<bool> editMastitisInDb(MastitisModel mastitis);
+  Future<bool> deleteMastitis(MastitisModel mastitis);
+  Future<bool> deleteAll();
+}
