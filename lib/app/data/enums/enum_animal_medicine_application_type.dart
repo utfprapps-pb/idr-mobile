c6cc875b02db0c60e310ebed551d27aa6319964e
marco-rozo <marcorozo99@gmail.com>
2023-05-23 00:04:22 -0300

    chore: create enum to application type

---
+++ lib/app/data/enums/enum_animal_medicine_application_type.dart
@@ -0,0 +1,8 @@
+enum AnimalMedicineApplicationType { im, iv, sc, pourOn }
+
+var typesApplication = {
+  AnimalMedicineApplicationType.im.name: "Intramuscular",
+  AnimalMedicineApplicationType.iv.name: "Intravenoso",
+  AnimalMedicineApplicationType.sc.name: "Subcutânea",
+  AnimalMedicineApplicationType.pourOn.name: "Pourn On",
+};
