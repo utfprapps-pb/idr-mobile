0bf633c02ecabd5202cdb00481ea459a0c1dd3ed
marco-rozo <marcorozo99@gmail.com>
2023-05-26 00:06:24 -0300

    feat: create a vegetable disease service

--- lib/app/data/services/vegetable_disease/vegetable_disease_service.dart
+++ lib/app/data/services/vegetable_disease/vegetable_disease_service.dart
@@ -1,10 +1,11 @@
-import 'package:idr_mobile/app/data/models/vegetable_plague_model.dart';
+import 'package:idr_mobile/app/data/models/vegetable_disease_model.dart';
 
-abstract class VegetablePlagueService {
-  Future<List<VegetablePlagueModel>> getAllVegetablePlagues(int? idProperty);
-  Future<bool> saveVegetablePlagues(List<VegetablePlagueModel> vegetablePlague);
-  Future<bool> saveVegetablePlague(VegetablePlagueModel vegetablePlague);
-  Future<bool> editVegetablePlague(VegetablePlagueModel vegetablePlague);
-  Future<bool> deleteVegetablePlague(VegetablePlagueModel vegetablePlague);
+abstract class VegetableDiseaseService {
+  Future<List<VegetableDiseaseModel>> getAllVegetableDiseases(int? idProperty);
+  Future<bool> saveVegetableDiseases(
+      List<VegetableDiseaseModel> vegetableDisease);
+  Future<bool> saveVegetableDisease(VegetableDiseaseModel vegetableDisease);
+  Future<bool> editVegetableDisease(VegetableDiseaseModel vegetableDisease);
+  Future<bool> deleteVegetableDisease(VegetableDiseaseModel vegetableDisease);
   Future<bool> deleteAll();
 }
--- lib/app/data/services/vegetable_disease/vegetable_disease_service_impl.dart
+++ lib/app/data/services/vegetable_disease/vegetable_disease_service_impl.dart
@@ -1,20 +1,23 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:idr_mobile/app/data/models/vegetable_disease_model.dart';
 import 'package:idr_mobile/app/data/models/vegetable_plague_model.dart';
+import 'package:idr_mobile/app/data/repositories/vegetable_disease/vegetable_disease_repository.dart';
 import 'package:idr_mobile/app/data/repositories/vegetable_plague/vegetable_plague_repository.dart';
+import 'package:idr_mobile/app/data/services/vegetable_disease/vegetable_disease_service.dart';
 import 'package:idr_mobile/app/data/services/vegetable_plague/vegetable_plague_service.dart';
 import 'package:uuid/uuid.dart';
 
-class VegetablePlagueServiceImpl implements VegetablePlagueService {
-  final VegetablePlagueRepository _vegetablePlagueRepository;
+class VegetableDiseaseServiceImpl implements VegetableDiseaseService {
+  final VegetableDiseaseRepository _vegetableDiseaseRepository;
   final Connectivity _connectivity;
   final Uuid _uuid;
 
-  VegetablePlagueServiceImpl({
-    required VegetablePlagueRepository vegetablePlagueRepository,
+  VegetableDiseaseServiceImpl({
+    required VegetableDiseaseRepository vegetableDiseaseRepository,
     required Connectivity connectivity,
     required Uuid uuid,
   })  : _connectivity = connectivity,
-        _vegetablePlagueRepository = vegetablePlagueRepository,
+        _vegetableDiseaseRepository = vegetableDiseaseRepository,
         _uuid = uuid;
 
   @override
@@ -24,34 +27,36 @@ class VegetablePlagueServiceImpl implements VegetablePlagueService {
   }
 
   @override
-  Future<bool> deleteVegetablePlague(VegetablePlagueModel vegetablePlague) =>
-      _vegetablePlagueRepository.deleteVegetablePlague(vegetablePlague);
+  Future<bool> deleteVegetableDisease(VegetableDiseaseModel vegetableDisease) =>
+      _vegetableDiseaseRepository.deleteVegetableDisease(vegetableDisease);
 
   @override
-  Future<bool> editVegetablePlague(VegetablePlagueModel vegetablePlague) =>
-      _vegetablePlagueRepository.editVegetablePlagueInDb(vegetablePlague);
+  Future<bool> editVegetableDisease(VegetableDiseaseModel vegetableDisease) =>
+      _vegetableDiseaseRepository.editVegetableDiseaseInDb(vegetableDisease);
 
   @override
-  Future<List<VegetablePlagueModel>> getAllVegetablePlagues(int? idProperty) =>
-      _vegetablePlagueRepository.getAllVegetablePlaguesInDb(idProperty);
+  Future<List<VegetableDiseaseModel>> getAllVegetableDiseases(
+          int? idProperty) =>
+      _vegetableDiseaseRepository.getAllVegetableDiseasesInDb(idProperty);
 
   @override
-  Future<bool> saveVegetablePlague(VegetablePlagueModel vegetablePlague) {
-    vegetablePlague.internalId ??= _uuid.v1();
+  Future<bool> saveVegetableDisease(VegetableDiseaseModel vegetableDisease) {
+    vegetableDisease.internalId ??= _uuid.v1();
 
-    return _vegetablePlagueRepository.saveVegetablePlagueInDb(vegetablePlague);
+    return _vegetableDiseaseRepository
+        .saveVegetableDiseaseInDb(vegetableDisease);
   }
 
   @override
-  Future<bool> saveVegetablePlagues(
-      List<VegetablePlagueModel> vegetablePlagues) {
-    for (var e in vegetablePlagues) {
+  Future<bool> saveVegetableDiseases(
+      List<VegetableDiseaseModel> vegetableDiseases) {
+    for (var e in vegetableDiseases) {
       {
         e.internalId ??= _uuid.v1();
       }
     }
 
-    return _vegetablePlagueRepository
-        .saveVegetablePlaguesInDb(vegetablePlagues);
+    return _vegetableDiseaseRepository
+        .saveVegetableDiseasesInDb(vegetableDiseases);
   }
 }
