b0e42ccf0a5625e52bbe46a1aef6e68740788061
marco-rozo <marcorozo99@gmail.com>
2023-05-25 22:53:20 -0300

    feat: create a disease repository to vegetable

--- lib/app/data/repositories/disease/disease_repository.dart
+++ lib/app/data/repositories/disease/disease_repository.dart
@@ -3,9 +3,7 @@ import 'package:idr_mobile/app/data/models/disease_model.dart';
 abstract class DiseaseRepository {
   Future<List<DiseaseModel>> getAllDiseases();
   Future<bool> saveDiseasesInDb(List<DiseaseModel> diseases);
-  Future<List<DiseaseModel>> getAllDiseasesInDb(String? animalIdentifier);
+  Future<List<DiseaseModel>> getAllDiseasesInDb();
   Future<bool> saveDiseaseInDb(DiseaseModel disease);
-  Future<bool> editDiseaseInDb(DiseaseModel disease);
-  Future<bool> deleteDisease(DiseaseModel disease);
   Future<bool> deleteAll();
 }
--- lib/app/data/repositories/disease/disease_repository_impl.dart
+++ lib/app/data/repositories/disease/disease_repository_impl.dart
@@ -1,128 +1,68 @@
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:idr_mobile/app/data/models/disease_model.dart';
-import 'package:idr_mobile/app/data/models/disease_model.dart';
 import 'package:idr_mobile/app/data/providers/api/rest_client.dart';
 import 'package:idr_mobile/app/data/providers/db/db.dart';
 import 'package:idr_mobile/app/data/repositories/disease/disease_repository.dart';
 import 'package:idr_mobile/app/data/repositories/disease/disease_repository.dart';
 import 'package:idr_mobile/app/data/services/auth/auth_service.dart';
+import 'package:idr_mobile/core/utils/header_api.dart';
 import 'package:idr_mobile/core/values/consts_db.dart';
-import 'package:uuid/uuid.dart';
 
 class DiseaseRepositoryImpl implements DiseaseRepository {
   final RestClient _restClient;
+  final AuthService auth;
   late Box _box;
 
   DiseaseRepositoryImpl({
     required RestClient restClient,
-  }) : _restClient = restClient;
+    required AuthService authService,
+  })  : _restClient = restClient,
+        auth = authService;
 
   @override
-  Future<bool> deleteAll() async {
-    var status = false;
-
-    try {
-      _box.delete(DISEASE);
-      status = true;
-    } catch (e) {
-      print(e);
-      status = false;
-    }
-
-    return status;
-  }
-
-  @override
-  Future<bool> deleteDisease(DiseaseModel disease) async {
-    var status = false;
-
-    try {
-      var diseases = _box.get(DISEASE) ?? [];
-      List<DiseaseModel> diseasesList =
-          diseases != null ? List<DiseaseModel>.from(diseases as List) : [];
-
-      diseasesList.remove(disease);
-
-      _box.put(DISEASE, diseasesList);
-
-      status = true;
-    } catch (e) {
-      print(e);
-      status = false;
-    }
-
-    return status;
-  }
-
-  @override
-  Future<bool> editDiseaseInDb(DiseaseModel disease) async {
-    var status = false;
-    try {
-      var diseases = _box.get(DISEASE) ?? [];
-
-      List<DiseaseModel> diseasesList =
-          diseases != null ? List<DiseaseModel>.from(diseases as List) : [];
-
-      List<DiseaseModel> list = [];
-      list.add(disease);
-
-      DiseaseModel? im = findDisease(diseasesList, disease);
-
-      int? pos = null;
-      if (im != null) {
-        pos = diseasesList.indexOf(im);
-      }
-
-      if (pos != null) {
-        diseasesList.replaceRange(pos, pos + 1, list);
-      }
-
-      _box.put(DISEASE, diseasesList);
-      status = true;
-    } catch (e) {
-      status = false;
-    }
-
-    return status;
-  }
-
-  @override
-  Future<List<DiseaseModel>> getAllDiseases() {
-    // TODO: implement getAllDiseases
+  Future<bool> deleteAll() {
+    // TODO: implement deleteAll
     throw UnimplementedError();
   }
 
   @override
-  Future<List<DiseaseModel>> getAllDiseasesInDb(
-      String? animalIdentifier) async {
+  Future<List<DiseaseModel>> getAllDiseases() async {
+    final result = await _restClient.get(
+      'diseases',
+      headers: HeadersAPI(token: auth.apiToken()).getHeaders(),
+      decoder: (data) {
+        final resultData = data;
+
+        if (resultData != null) {
+          var diseaseList = resultData
+              .map<DiseaseModel>((p) => DiseaseModel.fromMap(p))
+              .toList();
+
+          return diseaseList;
+        } else {
+          return <DiseaseModel>[];
+        }
+      },
+    );
+
+    // Caso houver erro
+    if (result.hasError) {
+      print('Error [${result.statusText}]');
+      throw Exception('Error _');
+    }
+
+    return result.body ?? <DiseaseModel>[];
+  }
+
+  @override
+  Future<List<DiseaseModel>> getAllDiseasesInDb() async {
     _box = await DatabaseInit().getInstance();
-    var diseases = _box.get(DISEASE) ?? [];
+    var diseases = _box.get(DISEASES) ?? [];
     List<DiseaseModel> diseasesList =
         diseases != null ? List<DiseaseModel>.from(diseases as List) : [];
 
-    if (animalIdentifier != null) {
-      diseasesList = findDiseaseByAnimal(animalIdentifier, diseasesList);
-    }
-
-    print(diseasesList);
-
     return diseasesList;
-  }
-
-  List<DiseaseModel> findDiseaseByAnimal(
-      String animalIdentifier, List<DiseaseModel> diseaseList) {
-    List<DiseaseModel> newList = [];
-
-    newList = diseaseList.where((o) {
-      if (o.animalIdentifier == animalIdentifier) {
-        return true;
-      }
-
-      return false;
-    }).toList();
-
-    return newList;
   }
 
   @override
@@ -131,10 +71,10 @@ class DiseaseRepositoryImpl implements DiseaseRepository {
     var status = false;
 
     try {
-      var diseases = _box.get(DISEASE) ?? [];
+      var diseases = _box.get(DISEASES) ?? [];
       diseases.add(disease);
 
-      _box.put(DISEASE, diseases);
+      _box.put(DISEASES, diseases);
       status = true;
     } catch (e) {
       print(e);
@@ -146,22 +86,16 @@ class DiseaseRepositoryImpl implements DiseaseRepository {
 
   @override
   Future<bool> saveDiseasesInDb(List<DiseaseModel> diseases) async {
-    _box = await DatabaseInit().getInstance();
+    var status = false;
 
-    // diseases.forEach((e) => {
-    //       if (e.internalId == null) {e.internalId = _uuid.v1()},
-    //     });
+    try {
+      _box.put(DISEASES, diseases);
+      status = true;
+    } catch (e) {
+      print(e);
+      status = false;
+    }
 
-    _box.put(DISEASE, diseases.toList());
-
-    return true;
-  }
-
-  DiseaseModel? findDisease(List<DiseaseModel> list, DiseaseModel disease) {
-    DiseaseModel? im = list.firstWhereOrNull(
-      (element) => element.internalId == disease.internalId,
-    );
-
-    return im;
+    return status;
   }
 }
