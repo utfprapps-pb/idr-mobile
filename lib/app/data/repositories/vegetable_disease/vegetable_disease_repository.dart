06e084c078997fc56a3bc5fd6adb010e3c1c25a3
marco-rozo <marcorozo99@gmail.com>
2023-05-26 00:02:25 -0300

    feat: create a vegetable disease repository

---
+++ lib/app/data/repositories/vegetable_disease/vegetable_disease_repository.dart
@@ -0,0 +1,13 @@
+import 'package:idr_mobile/app/data/models/vegetable_disease_model.dart';
+
+abstract class VegetableDiseaseRepository {
+  Future<List<VegetableDiseaseModel>> getAllVegetableDiseases();
+  Future<bool> saveVegetableDiseasesInDb(
+      List<VegetableDiseaseModel> vegetableDiseases);
+  Future<List<VegetableDiseaseModel>> getAllVegetableDiseasesInDb(
+      int? idProperty);
+  Future<bool> saveVegetableDiseaseInDb(VegetableDiseaseModel vegetableDisease);
+  Future<bool> editVegetableDiseaseInDb(VegetableDiseaseModel vegetableDisease);
+  Future<bool> deleteVegetableDisease(VegetableDiseaseModel vegetableDisease);
+  Future<bool> deleteAll();
+}
