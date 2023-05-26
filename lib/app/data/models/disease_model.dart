dc0e2b85260a2d7d98f145a1f61abbb5cee4f2aa
marco-rozo <marcorozo99@gmail.com>
2023-05-25 22:52:58 -0300

    feat: create a disease module to vegetable

--- lib/app/data/models/disease_model.dart
+++ lib/app/data/models/disease_model.dart
@@ -1,63 +1,49 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
+
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 part 'disease_model.g.dart';
 
-@HiveType(typeId: 5)
+@HiveType(typeId: 13)
 class DiseaseModel {
   @HiveField(1)
-  String? id;
-  @HiveField(2)
   String? internalId;
+  @HiveField(2)
+  int? id;
   @HiveField(3)
-  String? animalIdentifier;
-  @HiveField(4)
-  String? dateDiagnostic;
-  @HiveField(5)
-  String? diagnostic;
+  String? diseaseName;
 
   DiseaseModel({
-    this.id,
     this.internalId,
-    this.animalIdentifier,
-    this.dateDiagnostic,
-    this.diagnostic,
+    this.id,
+    this.diseaseName,
   });
 
   DiseaseModel copyWith({
-    String? id,
     String? internalId,
-    String? animalIdentifier,
-    String? dateDiagnostic,
-    String? diagnostic,
+    int? id,
+    String? diseaseName,
   }) {
     return DiseaseModel(
-      id: id ?? this.id,
       internalId: internalId ?? this.internalId,
-      animalIdentifier: animalIdentifier ?? this.animalIdentifier,
-      dateDiagnostic: dateDiagnostic ?? this.dateDiagnostic,
-      diagnostic: diagnostic ?? this.diagnostic,
+      id: id ?? this.id,
+      diseaseName: diseaseName ?? this.diseaseName,
     );
   }
 
   Map<String, dynamic> toMap() {
     final result = <String, dynamic>{};
 
-    if (id != null) {
-      result.addAll({'id': id});
-    }
     if (internalId != null) {
       result.addAll({'internalId': internalId});
     }
-    if (animalIdentifier != null) {
-      result.addAll({'animalIdentifier': animalIdentifier});
+    if (id != null) {
+      result.addAll({'id': id});
     }
-    if (dateDiagnostic != null) {
-      result.addAll({'dateDiagnostic': dateDiagnostic});
-    }
-    if (diagnostic != null) {
-      result.addAll({'diagnostic': diagnostic});
+    if (diseaseName != null) {
+      result.addAll({'diseaseName': diseaseName});
     }
 
     return result;
@@ -65,11 +51,9 @@ class DiseaseModel {
 
   factory DiseaseModel.fromMap(Map<String, dynamic> map) {
     return DiseaseModel(
-      id: map['id'],
       internalId: map['internalId'],
-      animalIdentifier: map['animalIdentifier'],
-      dateDiagnostic: map['dateDiagnostic'],
-      diagnostic: map['diagnostic'],
+      id: map['id']?.toInt(),
+      diseaseName: map['diseaseName'],
     );
   }
 
@@ -79,28 +63,18 @@ class DiseaseModel {
       DiseaseModel.fromMap(json.decode(source));
 
   @override
-  String toString() {
-    return 'DiseaseModel(id: $id, internalId: $internalId, animalIdentifier: $animalIdentifier, dateDiagnostic: $dateDiagnostic, diagnostic: $diagnostic)';
-  }
+  String toString() => '$id - $diseaseName';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
     return other is DiseaseModel &&
-        other.id == id &&
         other.internalId == internalId &&
-        other.animalIdentifier == animalIdentifier &&
-        other.dateDiagnostic == dateDiagnostic &&
-        other.diagnostic == diagnostic;
+        other.id == id &&
+        other.diseaseName == diseaseName;
   }
 
   @override
-  int get hashCode {
-    return id.hashCode ^
-        internalId.hashCode ^
-        animalIdentifier.hashCode ^
-        dateDiagnostic.hashCode ^
-        diagnostic.hashCode;
-  }
+  int get hashCode => internalId.hashCode ^ id.hashCode ^ diseaseName.hashCode;
 }
--- lib/app/data/models/disease_model.g.dart
+++ lib/app/data/models/disease_model.g.dart
@@ -8,7 +8,7 @@ part of 'disease_model.dart';
 
 class DiseaseModelAdapter extends TypeAdapter<DiseaseModel> {
   @override
-  final int typeId = 5;
+  final int typeId = 13;
 
   @override
   DiseaseModel read(BinaryReader reader) {
@@ -16,29 +16,22 @@ class DiseaseModelAdapter extends TypeAdapter<DiseaseModel> {
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return DiseaseModel(
-      id: fields[1] as String?,
-      internalId: fields[2] as String?,
-      animalIdentifier: fields[3] as String?,
-      dateDiagnostic: fields[4] as String?,
-      diagnostic: fields[5] as String?,
-    );
+    return DiseaseModel()
+      ..internalId = fields[1] as String?
+      ..id = fields[2] as int?
+      ..diseaseName = fields[3] as String?;
   }
 
   @override
   void write(BinaryWriter writer, DiseaseModel obj) {
     writer
-      ..writeByte(5)
-      ..writeByte(1)
-      ..write(obj.id)
-      ..writeByte(2)
-      ..write(obj.internalId)
       ..writeByte(3)
-      ..write(obj.animalIdentifier)
-      ..writeByte(4)
-      ..write(obj.dateDiagnostic)
-      ..writeByte(5)
-      ..write(obj.diagnostic);
+      ..writeByte(1)
+      ..write(obj.internalId)
+      ..writeByte(2)
+      ..write(obj.id)
+      ..writeByte(3)
+      ..write(obj.diseaseName);
   }
 
   @override
