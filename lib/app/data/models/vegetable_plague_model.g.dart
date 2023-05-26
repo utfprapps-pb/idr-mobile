b8ce060fabd7aadcfa8fa34eab424e4a2770dcc9
marco-rozo <marcorozo99@gmail.com>
2023-05-25 23:24:17 -0300

    feat: create a vegetable plagues model

--- lib/app/data/models/vegetable_plague_model.g.dart
+++ lib/app/data/models/vegetable_plague_model.g.dart
@@ -1,38 +1,50 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'culture_model.dart';
+part of 'vegetable_plague_model.dart';
 
 // **************************************************************************
 // TypeAdapterGenerator
 // **************************************************************************
 
-class CultureModelAdapter extends TypeAdapter<CultureModel> {
+class VegetablePlagueModelAdapter extends TypeAdapter<VegetablePlagueModel> {
   @override
-  final int typeId = 15;
+  final int typeId = 12;
 
   @override
-  CultureModel read(BinaryReader reader) {
+  VegetablePlagueModel read(BinaryReader reader) {
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return CultureModel(
+    return VegetablePlagueModel(
       internalId: fields[1] as String?,
       id: fields[2] as int?,
-      cultureName: fields[3] as String?,
+      infestationType: fields[3] as String?,
+      date: fields[4] as String?,
+      idProperty: fields[5] as int?,
+      idCulture: fields[6] as int?,
+      idPlague: fields[7] as int?,
     );
   }
 
   @override
-  void write(BinaryWriter writer, CultureModel obj) {
+  void write(BinaryWriter writer, VegetablePlagueModel obj) {
     writer
-      ..writeByte(3)
+      ..writeByte(7)
       ..writeByte(1)
       ..write(obj.internalId)
       ..writeByte(2)
       ..write(obj.id)
       ..writeByte(3)
-      ..write(obj.cultureName);
+      ..write(obj.infestationType)
+      ..writeByte(4)
+      ..write(obj.date)
+      ..writeByte(5)
+      ..write(obj.idProperty)
+      ..writeByte(6)
+      ..write(obj.idCulture)
+      ..writeByte(7)
+      ..write(obj.idPlague);
   }
 
   @override
@@ -41,7 +53,7 @@ class CultureModelAdapter extends TypeAdapter<CultureModel> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is CultureModelAdapter &&
+      other is VegetablePlagueModelAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
